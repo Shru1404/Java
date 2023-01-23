98d76a35f9029324ce75ada64684cbd115f9996b
Shru1404 <92007492+Shru1404@users.noreply.github.com>
2023-01-23 20:27:13 +0530

    Add files via upload

---
+++ practttical/First program.java
@@ -0,0 +1,11 @@
+classMy{
+public void display(){
+system.out.println("hi....");
+}
+}
+class Hello{ //start of class with main()
+public static void main(string args{}){//ontry point
+system.out.println("Its my first program.")//will print on console
+My.display();//classname.methodname
+}
+}//end of class
