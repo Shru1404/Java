c4b6bb9b86d60dd2cf2bdc47aaedf761a8e07767
Shru1404 <92007492+Shru1404@users.noreply.github.com>
2023-02-27 21:25:53 +0530

    Add files via upload

---
+++ practttical/StringEx.java
@@ -0,0 +1,16 @@
+public class StringEx{
+public static void main (String[] args){
+char[] c= {'s','o','n','u'};//Array of character
+String s= new String(c);
+String name= "Sonu";//String Declaration
+System.out.println(name);
+//Using Object Declaration
+String s1=new String ("ANudip");
+System.out.println(s1);
+//Using Literal Declaration
+String s2="Anudip";
+System.out.println(s2);
+}
+}
+
+
